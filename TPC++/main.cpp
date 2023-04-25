fd4ad45f58a6f5a443a6a6ff2e17f148a38649ca
Anas <131682794+Anas1010101@users.noreply.github.com>
2023-04-25 19:50:41 +0200

    Update main.cpp

--- TPC++/main.cpp
+++ TPC++/main.cpp
@@ -2,11 +2,24 @@
 #include "opencv2/highgui/highgui.hpp"
 #include <iostream>
 using namespace cv;
+using namespace std;
 
 int main(){
 Mat img=imread("myImage.jpg");
 Rect r=Rect(10,20,40,60);
 rectangle(img,r,Scalar(255,0,0),1,8,0);
 
+  
+Mat wee(200, 200, CV_8UC3, Scalar(255, 255, 255));
+Point center(100, 100);
+int radius = 50; 
+Scalar line_Color(0, 0, 0);
+int thickness = 2;
+namedWindow("whiteMatrix");
+circle(wee, center, radius, line_Color, thickness);
+imshow("WhiteMatrix", wee);
+waitKey(0);
+  
+ 
 return 0;
 }
