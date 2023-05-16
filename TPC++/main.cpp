ef12856ed0f44dc14dae9efaa165fd13e4474ce0
Anas1010101 <anasouadid11@gmail.com>
2023-05-16 11:29:30 +0200

    azerty

--- TPC++/main.cpp
+++ TPC++/main.cpp
@@ -13,24 +13,21 @@ using namespace std;
 // git push 
 // git log
 int main(){
-Mat image(200, 200, CV_8UC3, Scalar(255, 255, 255));
-Rect r=Rect(10,20,40,60);
-rectangle(image,r,Scalar(255,0,0),1,8,0);
+Mat fondBlanc(200, 200, CV_8UC3, Scalar(255, 255, 255));
+Rect rectangle_=Rect(10,20,40,60);
+rectangle(fondBlanc,rectangle_,Scalar(255,0,0),1,8,0);
 //coment
   
 
-namedWindow("whiteMatrix");
-imshow("WhiteMatrix", image);
-waitKey(0);
 
-Mat wee(200, 200, CV_8UC3, Scalar(255, 255, 255));
+
 Point center(100, 100);
 int radius = 50;
 Scalar line_Color(0, 0, 0);
 int thickness = 2;
 namedWindow("whiteMatrix");
-circle(wee, center, radius, line_Color, thickness);
-imshow("WhiteMatrix", wee);
+circle(fondBlanc, center, radius, line_Color, thickness);
+imshow("WhiteMatrix", fondBlanc);
 waitKey(0);
   
  
